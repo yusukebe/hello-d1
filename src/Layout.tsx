2dbc67f98093da9a6cd01cd9d89d41cd56781ac7
Yusuke Wada <yusuke@kamawada.com>
2022-07-31 21:42:24 +0900

    pretty

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,15 @@
-import { html } from 'hono/html'
+import { html } from "hono/html";
 
 export const Layout = (props: any) => html`<!DOCTYPE html>
   <html>
     <head>
       <title>Hello D1!</title>
-      <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
+      />
     </head>
     <body>
       <main class="container">${props.children}</main>
     </body>
-  </html>`
+  </html>`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,31 @@
-import { Hono } from 'hono'
-import { jsx } from 'hono/jsx'
-import type { Database } from '@cloudflare/d1'
-import { Top } from './Top'
-import type { Post } from './Top'
+import { Hono } from "hono";
+import { jsx } from "hono/jsx";
+import type { Database } from "@cloudflare/d1";
+import { Top } from "./Top";
+import type { Post } from "./Top";
 
 interface Env {
-  DB: Database
+  DB: Database;
 }
 
-const app = new Hono<Env>()
+const app = new Hono<Env>();
 
-app.get('/', async (c) => {
-  const response = await c.env.DB.prepare(`SELECT id,title,body FROM post`).all()
-  const posts: Array<Post> = response.results
-  return c.html(<Top posts={posts} />)
-})
+app.get("/", async (c) => {
+  const response = await c.env.DB.prepare(
+    `SELECT id,title,body FROM post`
+  ).all();
+  const posts: Array<Post> = response.results;
+  return c.html(<Top posts={posts} />);
+});
 
-app.post('/post', async (c) => {
-  const { title, body } = await c.req.parseBody()
+app.post("/post", async (c) => {
+  const { title, body } = await c.req.parseBody();
   if (title && body) {
-    await c.env.DB.prepare(`INSERT INTO post(title, body) VALUES(?, ?);`).bind(title, body).run()
+    await c.env.DB.prepare(`INSERT INTO post(title, body) VALUES(?, ?);`)
+      .bind(title, body)
+      .run();
   }
-  return c.redirect('/')
-})
+  return c.redirect("/");
+});
 
-export default app
+export default app;
