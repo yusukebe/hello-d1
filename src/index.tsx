a33344c5c447a8e13d724f9b5e58bf8207064fe5
Yusuke Wada <yusuke@kamawada.com>
2022-11-27 14:53:45 +0900

    support latest D1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { Hono } from 'hono'
 import { jsx } from 'hono/jsx'
-import { validation } from '@honojs/validator'
-import type { Database } from '@cloudflare/d1'
+import { validator } from 'hono/validator'
 import { Top } from './Top'
 import type { Post } from './types'
 
 interface Env {
-  DB: Database
+  DB: D1Database
 }
 
 const app = new Hono<{ Bindings: Env }>()
@@ -14,32 +13,33 @@ const app = new Hono<{ Bindings: Env }>()
 app.get('/', async (c) => {
   const { results } = await c.env.DB.prepare(
     `SELECT id,title,body FROM post;`
-  ).all()
-  const posts: Array<Post> = results
+  ).all<Post>()
+  const posts = results
   return c.html(<Top posts={posts} />)
 })
 
 app.post(
   '/post',
-  validation((v) => ({
-    body: {
-      title: [v.required],
-      body: [v.required],
-    },
-    done: (res, c) => {
-      if (res.hasError) {
-        return c.redirect('/')
-      }
-    },
-  }))
+  validator(
+    (v) => ({
+      title: v.body('title').isRequired(),
+      body: v.body('body').isRequired(),
+    }),
+    {
+      done: (res, c) => {
+        if (res.hasError) {
+          return c.redirect('/')
+        }
+      },
+    }
+  ),
+  async (c) => {
+    const { title, body } = c.req.valid()
+    await c.env.DB.prepare(`INSERT INTO post(title, body) VALUES(?, ?);`)
+      .bind(title, body)
+      .run()
+    return c.redirect('/')
+  }
 )
-
-app.post('/post', async (c) => {
-  const { title, body } = await c.req.parseBody()
-  await c.env.DB.prepare(`INSERT INTO post(title, body) VALUES(?, ?);`)
-    .bind(title, body)
-    .run()
-  return c.redirect('/')
-})
 
 export default app
